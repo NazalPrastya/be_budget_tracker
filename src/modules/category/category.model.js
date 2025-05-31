2ad4f74525aafce7aea4d89846f59cdc81590916
NazalPrastya <nazalprastya@gmail.com>
2025-05-31 11:44:29 +0700

    feat(category): add model category

---
+++ src/modules/category/category.model.js
@@ -0,0 +1,44 @@
+const sequelize = require("../../store/sequelize");
+
+module.export = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    "Category",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      tableName: "categories",
+      timestamps: true,
+    }
+  );
+
+  Category.associate = (models) => {
+    Category.hasMany(models.Transaction, {
+      foreginKey: "category_id",
+      as: "transaction",
+    });
+  };
+
+  return Category;
+};
