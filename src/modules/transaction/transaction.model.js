a4d933dea15741b2a6e64d695e44a684ad6636de
NazalPrastya <nazalprastya@gmail.com>
2025-06-02 00:34:01 +0700

    fix(transaction): handle fix createdAt using underscored

--- src/modules/transaction/transaction.model.js
+++ src/modules/transaction/transaction.model.js
@@ -53,6 +53,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       tableName: "transactions",
+      underscored: true,
       timestamps: true,
     }
   );
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 const userRouter = require("./modules/user/user.route");
 const authRouter = require("./modules/auth/auth.route");
+const categoryRouter = require("./modules/category/category.route");
+const transactionRoutes = require("./modules/transaction/transaction.route");
+const monthlySummaryRouter = require("./modules/monthlySummary/monthlySummary.route");
+const NotFound = require("./errors/NotFoundError");
 
+router.use("/categories", categoryRouter);
+router.use("/transaction", transactionRoutes);
+router.use("/monthly-summary", monthlySummaryRouter);
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
 
